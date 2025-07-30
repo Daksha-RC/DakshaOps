219fb03f0ad9345d6282bf5bd41a38d8dd305e31
Kiran Kumar Mallru <gmkumar2005@gmail.com>
2025-07-30 18:49:03 +0530

    GCP postgres with dev and sit schemas

--- index.ts
+++ index.ts
@@ -1,28 +1,62 @@
 import * as pulumi from "@pulumi/pulumi";
-import {createCloudSql} from "./stacks/cloudsql";
-import {CLOUD_SQL_NAME} from "./constants";
-import {createDefaultNetworkPeering} from "./stacks/networkpeering";
 import * as gcp from "@pulumi/gcp";
+import { createDbInstance, createDbUserAndDatabase } from "./stacks/cloudsql";
+import { createDefaultNetworkPeering } from "./stacks/networkpeering";
 
-// Get the configuration object for the 'db' namespace.
-const config = new pulumi.Config("db");
+// 0. Create the network peering. This must exist before the database.
+const networkPeer = createDefaultNetworkPeering(gcp.config.project!);
 
-// Retrieve database configuration from Pulumi config.
-const dbName = config.require("databaseName");
-const userName = config.require("databaseUserName");
-const dbPassword = config.requireSecret("pgPassword");
-// const networkPeer = createDefaultNetworkPeering(gcp.config.project);
-const networkPeer = createDefaultNetworkPeering(gcp.config.project ?? "default-project-id");
-// Create a new Cloud SQL instance using the factory function.
-const sqlInstance = createCloudSql(CLOUD_SQL_NAME, {
-    dbName: dbName,
-    userName: userName,
-    password: dbPassword,
-}, {
-    dependsOn: [networkPeer],
+// 1. Create a single, shared Cloud SQL Database Instance
+// This instance explicitly depends on the network peering connection being ready.
+const dbInstance = createDbInstance("shared-postgres-instance", {
+    dependsOn: [networkPeer.peeringConnection],
 });
 
-// Export the instance connection name to be used by applications.
-export const instanceConnectionName = sqlInstance.connectionName;
-export const databaseName = sqlInstance.databaseName;
-export const databaseUserName = sqlInstance.userName;
+// --- DEV Environment --- //
+
+// 2a. Get the configuration for the 'dev' environment
+const devConfig = new pulumi.Config("dev");
+const devDbName = devConfig.require("dbName");
+const devUserName = devConfig.require("userName");
+const devPassword = devConfig.requireSecret("password");
+
+// 3a. Create the 'dev' database and user
+// This resource explicitly depends on the dbInstance being ready.
+const devDb = createDbUserAndDatabase("dev-db-user", {
+    dbName: devDbName,
+    userName: devUserName,
+    password: devPassword,
+    dbInstance: dbInstance,
+    dependsOn: [dbInstance.instance,networkPeer.peeringConnection],
+});
+
+// --- SIT Environment --- //
+
+// 2b. Get the configuration for the 'sit' environment
+const sitConfig = new pulumi.Config("sit");
+const sitDbName = sitConfig.require("dbName");
+const sitUserName = sitConfig.require("userName");
+const sitPassword = sitConfig.requireSecret("password");
+
+// 3b. Create the 'sit' database and user
+// This resource also explicitly depends on the dbInstance being ready.
+const sitDb = createDbUserAndDatabase("sit-db-user", {
+    dbName: sitDbName,
+    userName: sitUserName,
+    password: sitPassword,
+    dbInstance: dbInstance,
+    dependsOn: [dbInstance.instance,networkPeer.peeringConnection],
+});
+
+// --- Exports --- //
+
+// Export the shared instance connection name
+export const instanceConnectionName = dbInstance.connectionName;
+
+// Export details for the 'dev' database
+export const devDatabaseName = devDb.databaseName;
+export const devDatabaseUserName = devDb.userName;
+
+// Export details for the 'sit' database
+export const sitDatabaseName = sitDb.databaseName;
+export const sitDatabaseUserName = sitDb.userName;
--- stacks/cloudsql.ts
+++ stacks/cloudsql.ts
@@ -1,29 +1,29 @@
-
 import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
 
-export interface CloudSqlArgs {
+// ---------------------------------------------------------------------------
+// Component 1: Database Instance
+// ---------------------------------------------------------------------------
+
+export interface DbInstanceArgs {
     project?: pulumi.Input<string>;
     region?: pulumi.Input<string>;
-    dbName: pulumi.Input<string>;
-    userName: pulumi.Input<string>;
-    password: pulumi.Input<string>;
     diskSizeGb?: pulumi.Input<number>;
+    dependsOn?: pulumi.Resource[];
 }
 
-export class CloudSql extends pulumi.ComponentResource {
+export class DbInstance extends pulumi.ComponentResource {
+    public readonly instance: gcp.sql.DatabaseInstance;
     public readonly instanceName: pulumi.Output<string>;
-    public readonly databaseName: pulumi.Output<string>;
-    public readonly userName: pulumi.Output<string>;
     public readonly connectionName: pulumi.Output<string>;
 
-    constructor(name: string, args: CloudSqlArgs, opts?: pulumi.ComponentResourceOptions) {
-        super("dakshaOps:component:CloudSql", name, {}, opts);
+    constructor(name: string, args: DbInstanceArgs, opts?: pulumi.ComponentResourceOptions) {
+        super("dakshaOps:component:DbInstance", name, {}, opts);
 
         const project = args.project ?? gcp.config.project;
         const region = args.region ?? gcp.config.region;
 
-        const instance = new gcp.sql.DatabaseInstance(`${name}-instance`, {
+        this.instance = new gcp.sql.DatabaseInstance(`${name}-instance`, {
             project: project,
             region: region,
             databaseVersion: "POSTGRES_17",
@@ -42,33 +42,70 @@ export class CloudSql extends pulumi.ComponentResource {
                     enabled: false,
                 },
             },
-        }, { parent: this });
+        }, { parent: this, dependsOn: args.dependsOn });
 
-        const database = new gcp.sql.Database(`${name}-db`, {
-            instance: instance.name,
-            name: args.dbName,
-        }, { parent: this });
-
-        const user = new gcp.sql.User(`${name}-user`, {
-            instance: instance.name,
-            name: args.userName,
-            password: args.password,
-        }, { parent: this });
-
-        this.instanceName = instance.name;
-        this.databaseName = database.name;
-        this.userName = user.name;
-        this.connectionName = instance.connectionName;
+        this.instanceName = this.instance.name;
+        this.connectionName = this.instance.connectionName;
 
         this.registerOutputs({
+            instance: this.instance,
             instanceName: this.instanceName,
-            databaseName: this.databaseName,
-            userName: this.userName,
             connectionName: this.connectionName,
         });
     }
 }
 
-export function createCloudSql(name: string, args: CloudSqlArgs, opts?: pulumi.ComponentResourceOptions): CloudSql {
-    return new CloudSql(name, args, opts);
+/**
+ * Factory function to create a Cloud SQL database instance.
+ */
+export function createDbInstance(name: string, args: DbInstanceArgs, opts?: pulumi.ComponentResourceOptions): DbInstance {
+    return new DbInstance(name, args, opts);
+}
+
+
+// ---------------------------------------------------------------------------
+// Component 2: Database and User
+// ---------------------------------------------------------------------------
+
+export interface DbUserAndDatabaseArgs {
+    dbName: pulumi.Input<string>;
+    userName: pulumi.Input<string>;
+    password: pulumi.Input<string>;
+    dbInstance: DbInstance; // Pass the instance component as a dependency
+    dependsOn?: pulumi.Resource[];
+}
+
+export class DbUserAndDatabase extends pulumi.ComponentResource {
+    public readonly databaseName: pulumi.Output<string>;
+    public readonly userName: pulumi.Output<string>;
+
+    constructor(name: string, args: DbUserAndDatabaseArgs, opts?: pulumi.ComponentResourceOptions) {
+        super("dakshaOps:component:DbUserAndDatabase", name, {}, opts);
+
+        const database = new gcp.sql.Database(`${name}-db`, {
+            instance: args.dbInstance.instanceName,
+            name: args.dbName,
+        }, { parent: this, dependsOn: args.dependsOn });
+
+        const user = new gcp.sql.User(`${name}-user`, {
+            instance: args.dbInstance.instanceName,
+            name: args.userName,
+            password: args.password,
+        }, { parent: this, dependsOn: args.dependsOn });
+
+        this.databaseName = database.name;
+        this.userName = user.name;
+
+        this.registerOutputs({
+            databaseName: this.databaseName,
+            userName: this.userName,
+        });
+    }
+}
+
+/**
+ * Factory function to create a database and user for a given Cloud SQL instance.
+ */
+export function createDbUserAndDatabase(name: string, args: DbUserAndDatabaseArgs, opts?: pulumi.ComponentResourceOptions): DbUserAndDatabase {
+    return new DbUserAndDatabase(name, args, opts);
 }
